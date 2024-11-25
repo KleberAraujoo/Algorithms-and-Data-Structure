726ebfc1a1520f19de36b6d1b0cd8b8298c76784
Kleberson Araujo <112328070+KleberAraujoo@users.noreply.github.com>
2024-11-25 20:25:20 -0300

    Update bubble_sort.cpp

--- studies/bubble_sort.cpp
+++ studies/bubble_sort.cpp
@@ -22,9 +22,9 @@ void sort(int array[], int size) {
 
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
-
+            
             // troca de elementos
-            if (array[j] > array[j + 1])  // verifica se o valor em questão do array é maior que valor seguinte do array, se sim, entra no if
+            if (array[j] > array[j + 1]) { // verifica se o valor em questão do array é maior que valor seguinte do array, se sim, entra no if
                 int temp = array[j];    // armazena o valor do array em uma variável temporária
                 array[j] = array[j + 1]; // o valor do array atual recebe o proximo valor do array
                 array[j + 1] = temp; // o proximo valor do array recebe a variável temporária
@@ -33,6 +33,7 @@ void sort(int array[], int size) {
             // como array[j] é maior que array[j+1] então entra no if
             // temp recebe 5 do array[j] e o array [j] recebe 2 do array[j+1]
             // array[j+1] recebe 5 do temp, ficando assim {1,5}
+            }
         }
     }
 }
